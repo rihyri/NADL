90538c13e1f2796f0598cfeb88a615a4a3e4e18a
rihyri <rihyri.dev@gmail.com>
2026-06-04 20:48:13 +0900

    chore : 프로젝트 구조 backend 폴더로 정리

---
+++ backend/src/main/java/com/rihyri/NADL/NadlApplication.java
@@ -0,0 +1,13 @@
+package com.rihyri.NADL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NadlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NadlApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rihyri/NADL/NadlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rihyri.NADL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NadlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
